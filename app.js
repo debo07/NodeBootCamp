2e398fc600eaa919c17c3c0e7ea2e4b6ed4a059a
Debajit Majumder <debajit.business@gmail.com>
2021-04-03 16:56:28 +0530

    Added bootcamp routes and basic response controllers

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+//Routes
+const bootcamps = require("./routes/bootcamps");
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const server = express();
+server.use("/api/v1/bootcamps", bootcamps);
 
 PORT = process.env.PORT || 5000;
-
 server.listen(
   PORT,
   console.log(`Server is running in ${process.env.ENV} mode at ${PORT} port`)
