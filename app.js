e060e93f210813fa33ff6f176ed08b721671fbf1
Debajit Majumder <debajit.business@gmail.com>
2021-04-03 19:26:40 +0530

    Connected to mongo db

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 //Routes
 const bootcamps = require("./routes/bootcamps");
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//connect to database
+connectDB();
+//Initialize express server
 const app = express();
+
+if (process.env.ENV === "development") {
+    app.use(morgan("dev"));
+}
 app.use("/api/v1/bootcamps", bootcamps);
 
 PORT = process.env.PORT || 5000;
