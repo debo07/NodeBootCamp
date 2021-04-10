a53678e2412e8c3f1f8250bfc15ca7e130d6c363
Debajit Majumder <debajit.business@gmail.com>
2021-04-10 20:16:03 +0530

    Few more error scenarios handled at common error handler

--- middleware/error.js
+++ middleware/error.js
@@ -10,10 +10,22 @@ const errorHandler = (err, req, res, next) => {
 
     if (err.name === "CastError") {
         error = new ErrorResponse(
-            `CastError: Bootcamp not found with id: ${err.value}`,
+            `Bootcamp not found with id: ${err.value}`,
             404,
         );
     }
+
+    if (err.name === "MongoError" && err.code === 11000) {
+        error = new ErrorResponse("Duplicate field value entered", 400);
+    }
+
+    if (err.name === "ValidationError") {
+        const combinedMessage = Object.values(err.errors).map(
+            val => val.message,
+        );
+        error = new ErrorResponse(combinedMessage, 400);
+    }
+
     res.status(error.statusCode || 500).json({
         status: false,
         message: error.message || "Internal server error",
